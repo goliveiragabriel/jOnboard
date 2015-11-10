5c94c51e56b14d6921ffacb78e8e04241783b87c
unknown <gabriel.goncalves@metdes0267.metaora.com.br>
2015-11-10 14:19:15 -0200

    My first commit

---
+++ Scripts/SignalR.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    onboardHub = $.connection.onBoardHub;
+    function init() {
+
+    }
+    onboardHub.client.broadcastMessage = function (x, y, dragging) {
+        canvas.addClick(x, y, dragging);
+        canvas.redraw();
+    }
+    $.connection.hub.start().done(init);
+}());
